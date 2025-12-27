fab3bce4152747137d74ff4f10a50eab42c1755c
Datta Bunde <dattabunde09@gmail.com>
2025-12-27 20:14:01 +0530

    Switch database from MySQL to PostgreSQL (Render)

--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -68,23 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ticket_system.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ticket_system_db',
-#         'USER': 'root',        # update if you use a different MySQL user
-#         'PASSWORD': 'tiger',        # provide the actual password
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
