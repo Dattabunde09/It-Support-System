0fb97230a7c98f6ba5427af4c296d3f02f8d24b5
Datta Bunde <dattabunde09@gmail.com>
2025-12-28 15:03:55 +0530

    Switch to console email backend for debugging

--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -150,9 +150,9 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 # ================= EMAIL SETTINGS =================
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'ticketsystem.mail09@gmail.com'
